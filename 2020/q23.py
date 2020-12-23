4d481e5609f7e2b9313ce5f198d396200f4c98f6
kevinc <kevinc@cinesite.com>
2020-12-23 22:50:23 +1030

    q23: crabby games with many cards (dict based)

--- 2020/q23.py
+++ 2020/q23.py
@@ -2,70 +2,56 @@
 start = '315679824'
 
 
-class Cup:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-
-def print_cups(cup):
-    print('cups: ', cup.value, end='')
-    n = cup.next
-    while n.value != cup.value:
-        print(n.value, end='')
-        n = n.next
+def print_cups(cup_dict, first, limit=10):
+    print('cups: ', first, end='')
+    n = cup_dict[first]
+    cnt = 0
+    while n != first and cnt < limit:
+        print(n, end='')
+        n = cup_dict[n]
+        cnt += 1
     print()
 
 
-#
-# build linked list on top of a hash for faster indexing
-#
-cups = {}
-previous = None
+cups = {}  # dictionary with the next number as the value
 current = None
-for c in [int(d) for d in start]:
-    cups[c] = Cup(c)
+last = None
+for c in [int(d) for d in start] + list(range(len(start) + 1, 1000 * 1000 + 1)):
     if not current:
-        current = cups[c]
-    if previous:
-        previous.next = cups[c]
-    previous = cups[c]
-for c in range(len(start) + 1, 1000001):
-    cups[c] = Cup(c)
-    previous.next = cups[c]
-    previous = cups[c]
-previous.next = current
+        current = c
+    if last:
+        cups[last] = c
+    last = c
+cups[last] = current
 
 
 most = max(cups.keys())
-for move in range(10000000):
-    if move % 100000 == 0:
-        print('move:', move + 1)
-    # print_cups(current)
+for move in range(10 * 1000 * 1000):
+    # print('move:', move + 1)
+    # print_cups(cups, current)
 
     # build pickup
-    pickup = [current.next, current.next.next, current.next.next.next]
-    pickup_values = {p.value for p in pickup}
+    pickup = [cups[current],
+              cups[cups[current]],
+              cups[cups[cups[current]]]]
     # remove pickup
-    current.next = current.next.next.next.next
+    cups[current] = cups[cups[cups[cups[current]]]]
 
     # find destination
-    destination = current.value - 1
+    destination = current - 1
     if destination == 0:
         destination = most
-    while destination in pickup_values:
+    while destination in pickup:
         destination = destination - 1
         if destination == 0:
             destination = most
     # print('destination:', destination)
 
     # insert pickup which is already chained together
-    end = cups[destination].next
-    cups[destination].next = pickup[0]
-    pickup[-1].next = end
+    cups[destination], cups[pickup[-1]] = pickup[0], cups[destination]
 
     # next
-    current = current.next
+    current = cups[current]
 
-# print_cups(cups[1])
-print('result part two:', cups[1].next.value * cups[1].next.next.value)
+print_cups(cups, 1)
+print('result part two:', cups[1] * cups[cups[1]])
