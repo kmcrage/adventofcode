61e435908fcda472ffa8f6b8f059cbd1db783213
kevin campbell <kevin.campbellrage@gmail.com>
2021-12-01 16:10:18 +1030

    aoc 2021 day 1

---
+++ 2021/2021-12-01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+filename = '2021-12-01.dat'
+
+
+def part_one():
+    increments = 0
+    last = 9999999
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            depth = int(line)
+            if depth > last:
+                increments += 1
+            last = depth
+    print(increments)
+
+
+def part_two(length=3):
+    increments = 0
+    last = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            depth = int(line)
+            last.append(depth)
+            if len(last) > length and last.pop(0) < depth:
+                increments += 1
+    print(increments)
+
+
+part_two()
