6eedac0970bbcd734b1f602f1cc57519b0ade13a
kevin campbell <kevin.campbellrage@gmail.com>
2021-12-21 22:07:43 +1030

    tidy

--- 2021/2021-12-21.py
+++ 2021/2021-12-21.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from collections import Counter
+import itertools
+
 
 def part_one(win):
     pos = [3, 4]
@@ -17,46 +20,44 @@ def part_one(win):
     print("part one:", min(score) * 3 * round)
 
 
-def get_dice():
-    dice = {}
-    for i in [1, 2, 3]:
-        for j in [1, 2, 3]:
-            for k in [1, 2, 3]:
-                s = i + j + k
-                dice[s] = dice.get(s, 0) + 1
-    return dice
+def get_dice(rolls):
+    return Counter(sum(p) for p in itertools.product([1, 2, 3], repeat=rolls))
 
 
-def part_two(universe, win):
-    dice = get_dice()
+def part_two(multiverse, win):
+    dice = get_dice(3)
     player = 0
-    while min(max(u[1], u[3]) for u in universe) < win:
-        universe_prev = universe
-        universe = {}
-        for u, num_u in universe_prev.items():
-            if u[1] >= win or u[3] >= win:
-                universe[u] = universe.get(u, 0) + num_u
+    while min(max(u[p][1] for p in [0, 1]) for u in multiverse) < win:
+        multiverse_prev = multiverse
+        multiverse = {}
+        for u, num_u in multiverse_prev.items():
+            if any(u[p][1] >= win for p in [0, 1]):
+                multiverse[u] = multiverse.get(u, 0) + num_u
                 continue
 
             for die, num_die in dice.items():
-                v = list(u)
-                v[2 * player] = (v[2 * player] + die - 1) % 10 + 1
-                v[2 * player + 1] += v[2 * player]
+                v = [None, None]
+                v[1 - player] = u[1 - player]
+
+                position = (u[player][0] + die - 1) % 10 + 1
+                score = u[player][1] + position
+                v[player] = (position, score)
                 v = tuple(v)
-                universe[v] = universe.get(v, 0) + num_die * num_u
+
+                multiverse[v] = multiverse.get(v, 0) + num_die * num_u
 
         player = 1 - player
-    return universe
+    return multiverse
 
 
 part_one(1000)
 print()
 
-# [{pos_a, score_a , pos_b, score_b) : num of universes}, ....]
+# [((pos_a, score_a) , (pos_b, score_b)) : num of universes, ....]
 win = 21
-universe = {(3, 0, 4, 0): 1}
-universe = part_two(universe, win)
-wins = [sum(n for u, n in universe.items() if u[p] >= win) for p in [1, 3]]
+multiverse = {((3, 0), (4, 0)): 1}
+multiverse = part_two(multiverse, win)
+wins = [sum(n for u, n in multiverse.items() if u[p][1] >= win) for p in [0, 1]]
 print("player one wins:", wins[0])
 print("player two wins:", wins[1])
 print("max wins:", max(wins))
