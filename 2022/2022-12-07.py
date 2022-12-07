822c95d001e19f192b56da47c23da6d551972b55
kevin campbell <kevin.campbellrage@gmail.com>
2022-12-07 19:02:29 +1030

    day7:cleaner solution

--- 2022/2022-12-07.py
+++ 2022/2022-12-07.py
@@ -4,7 +4,7 @@ data_filename = "2022-12-07.dat"
 # data_filename = "test.dat"
 
 
-def part_one(filename, max_size):
+def parser(filename):
     filesystem = {}
     cwd = None
     with open(filename, "r") as f:
@@ -12,6 +12,8 @@ def part_one(filename, max_size):
         for line in f:
             # print(line)
             tokens = line.split()
+
+            # navigate
             if tokens[1] == "cd":
                 if tokens[2] == "/":
                     cwd = "/"
@@ -22,51 +24,30 @@ def part_one(filename, max_size):
                 if cwd not in filesystem:
                     filesystem[cwd] = 0
 
+            # file sizes
             elif tokens[0] not in ("$", "dir"):
                 dr = cwd
                 while dr:
                     filesystem[dr] += int(tokens[0])
                     dr = "/".join(dr.split("/")[:-1])
+    return filesystem
 
-            # print("cwd:", cwd)
+
+def part_one(filesystem, max_size):
     result = sum(s for s in filesystem.values() if s <= max_size)
-    print(f"part one: {result}")
+    print(f"part one, small directories: {result}")
 
 
-def part_two(filename, total, target):
-    filesystem = {}
-    cwd = None
-    with open(filename, "r") as f:
-        # parse
-        for line in f:
-            # print(line)
-            tokens = line.split()
-            if tokens[1] == "cd":
-                if tokens[2] == "/":
-                    cwd = "/"
-                elif tokens[2] == "..":
-                    cwd = "/".join(cwd.split("/")[:-1])
-                else:
-                    cwd += f"/{tokens[2]}"
-                if cwd not in filesystem:
-                    filesystem[cwd] = 0
-
-            elif tokens[0] not in ("$", "dir"):
-                dr = cwd
-                while dr:
-                    filesystem[dr] += int(tokens[0])
-                    dr = "/".join(dr.split("/")[:-1])
-
-            # print("cwd:", cwd)
-    print(f'filesystem: {filesystem["/"]}')
+def part_two(filesystem, total, target):
     removal = target + filesystem["/"] - total
-    print(f"remove: {removal}")
+    # print(f"remove: {removal}")
     min_removal = total
     for s in filesystem.values():
         if s < min_removal and s >= removal:
             min_removal = s
-    print(f"part two: {min_removal}")
+    print(f"part two, minimal removal: {min_removal}")
 
 
-part_one(data_filename, 100_000)
-part_two(data_filename, 70_000_000, 30_000_000)
+filesystem = parser(data_filename)
+part_one(filesystem, 100_000)
+part_two(filesystem, 70_000_000, 30_000_000)
