6092387dd60a1d3f722b949492aa100cf87ab520
kevin campbell <kevin.campbellrage@gmail.com>
2022-12-09 16:25:15 +1030

    day9: black

--- 2022/2022-12-09.py
+++ 2022/2022-12-09.py
@@ -10,7 +10,7 @@ DIRNS = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 def part_one(filename):
     h_pos = [0, 0]
     t_pos = [0, 0]
-    
+
     locations = {tuple(t_pos)}
     with open(filename, "r") as f:
         for line in f:
@@ -44,19 +44,24 @@ def part_two(filename, knots):
                 pos[0] = (pos[0][0] + DIRNS[dirn][0], pos[0][1] + DIRNS[dirn][1])
 
                 # tail motion: knot (n) follows knot (n-1)
-                for num in range(1,knots):
-                    if abs(pos[num-1][0] - pos[num][0]) + abs(pos[num-1][1] - pos[num][1]) > 2:
-                        pos[num][0] += math.copysign(1, pos[num-1][0] - pos[num][0])
-                        pos[num][1] += math.copysign(1, pos[num-1][1] - pos[num][1])
-                    elif abs(pos[num-1][0] - pos[num][0]) == 2:
-                        pos[num][0] += math.copysign(1, pos[num-1][0] - pos[num][0])
-                    elif abs(pos[num-1][1] - pos[num][1]) == 2:
-                        pos[num][1] += math.copysign(1, pos[num-1][1] - pos[num][1])
+                for num in range(1, knots):
+                    if (
+                        abs(pos[num - 1][0] - pos[num][0])
+                        + abs(pos[num - 1][1] - pos[num][1])
+                        > 2
+                    ):
+                        pos[num][0] += math.copysign(1, pos[num - 1][0] - pos[num][0])
+                        pos[num][1] += math.copysign(1, pos[num - 1][1] - pos[num][1])
+                    elif abs(pos[num - 1][0] - pos[num][0]) == 2:
+                        pos[num][0] += math.copysign(1, pos[num - 1][0] - pos[num][0])
+                    elif abs(pos[num - 1][1] - pos[num][1]) == 2:
+                        pos[num][1] += math.copysign(1, pos[num - 1][1] - pos[num][1])
 
                 # track the location of the last knot
                 locations.add(tuple(pos[-1]))
 
     print(knots, "knot locations:", len(locations))
+
 
 part_one(data_filename)
 part_two(data_filename, 2)
