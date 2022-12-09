7267b108d07e85906c19496ef825a57028b62d5e
kevin campbell <kevin.campbellrage@gmail.com>
2022-12-09 16:57:04 +1030

    day9: factor out move

--- 2022/2022-12-09.py
+++ 2022/2022-12-09.py
@@ -10,6 +10,16 @@ def sign(num):
     return (num > 0) - (num < 0)
 
 
+def move_tail(tail, head):
+    if abs(head[0] - tail[0]) + abs(head[1] - tail[1]) > 2:
+        tail[0] += sign(head[0] - tail[0])
+        tail[1] += sign(head[1] - tail[1])
+    elif abs(head[0] - tail[0]) == 2:
+        tail[0] += sign(head[0] - tail[0])
+    elif abs(head[1] - tail[1]) == 2:
+        tail[1] += sign(head[1] - tail[1])
+
+
 def part_one(filename):
     h_pos = [0, 0]
     t_pos = [0, 0]
@@ -20,15 +30,7 @@ def part_one(filename):
             dirn, dist = line.strip().split()
             for _ in range(int(dist)):
                 h_pos = [h_pos[0] + DIRNS[dirn][0], h_pos[1] + DIRNS[dirn][1]]
-
-                if abs(h_pos[0] - t_pos[0]) + abs(h_pos[1] - t_pos[1]) > 2:
-                    t_pos[0] += sign(h_pos[0] - t_pos[0])
-                    t_pos[1] += sign(h_pos[1] - t_pos[1])
-                elif abs(h_pos[0] - t_pos[0]) == 2:
-                    t_pos[0] += sign(h_pos[0] - t_pos[0])
-                elif abs(h_pos[1] - t_pos[1]) == 2:
-                    t_pos[1] += sign(h_pos[1] - t_pos[1])
-
+                move_tail(t_pos, h_pos)
                 locations.add(tuple(t_pos))
 
     print("two knot locations:", len(locations))
@@ -45,21 +47,9 @@ def part_two(filename, knots):
             for _ in range(int(dist)):
                 # head motion
                 pos[0] = (pos[0][0] + DIRNS[dirn][0], pos[0][1] + DIRNS[dirn][1])
-
                 # tail motion: knot (n) follows knot (n-1)
                 for num in range(1, knots):
-                    if (
-                        abs(pos[num - 1][0] - pos[num][0])
-                        + abs(pos[num - 1][1] - pos[num][1])
-                        > 2
-                    ):
-                        pos[num][0] += sign(pos[num - 1][0] - pos[num][0])
-                        pos[num][1] += sign(pos[num - 1][1] - pos[num][1])
-                    elif abs(pos[num - 1][0] - pos[num][0]) == 2:
-                        pos[num][0] += sign(pos[num - 1][0] - pos[num][0])
-                    elif abs(pos[num - 1][1] - pos[num][1]) == 2:
-                        pos[num][1] += sign(pos[num - 1][1] - pos[num][1])
-
+                    move_tail(pos[num], pos[num - 1])
                 # track the location of the last knot
                 locations.add(tuple(pos[-1]))
 
