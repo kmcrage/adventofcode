8587de12c15250d0fae7bfe08f42be0a33fcac6a
kevin campbell <kevin.campbellrage@gmail.com>
2022-12-16 12:14:32 +1030

    day15: merge intervals for part one

--- 2022/2022-12-15.py
+++ 2022/2022-12-15.py
@@ -7,7 +7,7 @@ data_filename = "2022-12-15.dat"
 # data_filename = "test.dat"
 
 
-def part_one(filename, row):
+def part_one_bf(filename, row):
     scan_row = {}
     with open(filename, "r") as f:
         for line in f:
@@ -26,6 +26,42 @@ def part_one(filename, row):
                 if x not in scan_row:
                     scan_row[x] = "#"
     result = len([s for s in scan_row if scan_row[s] == "#"])
+    print(f"part one positions {result}")
+
+
+def get_intervals(filename, row):
+    intervals = []
+    beacons = set()
+    with open(filename, "r") as f:
+        for line in f:
+            m = re.search(r"x=(-?\d+), y=(-?\d+).*x=(-?\d+), y=(-?\d+)", line)
+            if not m:
+                continue
+            sensor = int(m[1]), int(m[2])
+            beacon = int(m[3]), int(m[4])
+            if beacon[1] == row:
+                beacons.add(beacon)
+            m_dist = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+            h = abs(row - sensor[1])
+            intervals.append([sensor[0] - m_dist + h, sensor[0] + m_dist - h])
+    return intervals, len(beacons)
+
+
+def merge_intervals(intervals):
+    intervals = sorted(intervals)
+    result = intervals[:1]
+    for interval in intervals[1:]:
+        if interval[0] > result[-1][1]:
+            result.append(interval)
+            continue
+        result[-1][1] = max(result[-1][1], interval[1])
+    return result
+
+
+def part_one_intervals(filename, row):
+    intervals, beacons = get_intervals(filename, row)
+    intervals = merge_intervals(intervals)
+    result = sum(interval[1] - interval[0] + 1 for interval in intervals) - beacons
     print(f"part one positions {result}")
 
 
@@ -97,23 +133,19 @@ def intersections(disks, maxsize):
     for disk_a, disk_b in itertools.combinations(disks, 2):
         disk_a_skew = (disk_a[0][0] + disk_a[0][1], disk_a[0][0] - disk_a[0][1])
         disk_b_skew = (disk_b[0][0] + disk_b[0][1], disk_b[0][0] - disk_b[0][1])
+        candidate_max = (
+            max(disk_a_skew[0] - disk_a[1] - 1, disk_b_skew[0] - disk_b[1] - 1),
+            max(disk_a_skew[1] - disk_a[1] - 1, disk_b_skew[1] - disk_b[1] - 1),
+        )
+        candidate_min = (
+            min(disk_a_skew[0] + disk_a[1] + 1, disk_b_skew[0] + disk_b[1] + 1),
+            min(disk_a_skew[1] + disk_a[1] + 1, disk_b_skew[1] + disk_b[1] + 1),
+        )
         for candidate_skew in (
-            (
-                max(disk_a_skew[0] - disk_a[1] - 1, disk_b_skew[0] - disk_b[1] - 1),
-                max(disk_a_skew[1] - disk_a[1] - 1, disk_b_skew[1] - disk_b[1] - 1),
-            ),
-            (
-                max(disk_a_skew[0] - disk_a[1] - 1, disk_b_skew[0] - disk_b[1] - 1),
-                min(disk_a_skew[1] + disk_a[1] + 1, disk_b_skew[1] + disk_b[1] + 1),
-            ),
-            (
-                min(disk_a_skew[0] + disk_a[1] + 1, disk_b_skew[0] + disk_b[1] + 1),
-                max(disk_a_skew[1] - disk_a[1] - 1, disk_b_skew[1] - disk_b[1] - 1),
-            ),
-            (
-                min(disk_a_skew[0] + disk_a[1] + 1, disk_b_skew[0] + disk_b[1] + 1),
-                min(disk_a_skew[1] + disk_a[1] + 1, disk_b_skew[1] + disk_b[1] + 1),
-            ),
+            candidate_max,
+            (candidate_max[0], candidate_min[1]),
+            (candidate_min[0], candidate_max[1]),
+            candidate_min,
         ):
             candidate = (
                 (candidate_skew[0] + candidate_skew[1]) // 2,
@@ -141,7 +173,8 @@ def skew_coords(disks, maxsize):
     print("part two frequency:", result[0] * 4_000_000 + result[1])
 
 
-part_one(data_filename, 2_000_000)
+# part_one_bf(data_filename, 2_000_000)
+part_one_intervals(data_filename, 2_000_000)
 disks = parser(data_filename)
 # perimeters_bf(disks, 4_000_000)
 skew_coords(disks, 4_000_000)
