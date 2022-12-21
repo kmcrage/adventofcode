a4a6df9d11ea2e136898ce206d186608e1385ab3
kevin campbell <kevin.campbellrage@gmail.com>
2022-12-21 12:33:42 +1030

    day19: optimise

--- 2022/2022-12-19.py
+++ 2022/2022-12-19.py
@@ -44,13 +44,8 @@ def get_blueprints(filename):
 
 
 def estimax(nhbr):
-    time, resources, robots = nhbr
-    return resources[3] + robots[3] * time + time * (time - 1) // 2
-
-
-def estimin(nhbr):
-    time, resources, robots = nhbr
-    return resources[3] + robots[3] * time
+    time, _, _, gas = nhbr
+    return gas + time * (time - 1) // 2
 
 
 def bfs(blueprint, resources, robots, maxtime):
@@ -68,16 +63,26 @@ def bfs(blueprint, resources, robots, maxtime):
         _type_: _description_
     """
     visited = {resources, robots}
-    queue = collections.deque([(maxtime, resources, robots)])
+    queue = collections.deque([(maxtime, resources, robots, 0)])
+    max_costs = [0, 0, 0, 0]
+    for cost in blueprint:
+        for r in range(3):
+            if cost[r] > max_costs[r]:
+                max_costs[r] = cost[r]
 
     best = 0
     while queue:  # Creating loop to visit each node
-        time, resources, robots = nhbr = queue.popleft()
+        time, resources, robots, gas = nhbr = queue.popleft()
         if time <= 0 or estimax(nhbr) <= best:
             continue
 
         for r, cost in enumerate(blueprint):
+            if r < 3 and robots[r] >= max_costs[r]:
+                # don't make more robots than we can use
+                continue
+
             nhbr_time = time - 1
+            nhbr_gas = gas
             nhbr_resources = [
                 resources[0] - cost[0],
                 resources[1] - cost[1],
@@ -85,14 +90,27 @@ def bfs(blueprint, resources, robots, maxtime):
                 resources[3] - cost[3],
             ]
             nhbr_robots = robots
-            while nhbr_time > 0 and (
-                nhbr_resources[0] < 0 or nhbr_resources[1] < 0 or nhbr_resources[2] < 0
+            if (
+                (nhbr_resources[0] < 0 and nhbr_robots[0] == 0)
+                or (nhbr_resources[1] < 0 and nhbr_robots[1] == 0)
+                or (nhbr_resources[2] < 0 and nhbr_robots[2] == 0)
             ):
-                nhbr_time -= 1
-                nhbr_resources[0] += robots[0]
-                nhbr_resources[1] += robots[1]
-                nhbr_resources[2] += robots[2]
-                nhbr_resources[3] += robots[3]
+                continue
+
+            # take enough time steps to create the next robot
+            steps = 0
+            for res in (0, 1, 2, 3):
+                if nhbr_resources[res] < 0:
+                    steps_r = -(nhbr_resources[res] // nhbr_robots[res])
+                    if steps_r > steps:
+                        steps = steps_r
+            nhbr_time -= steps
+            nhbr_resources = (
+                nhbr_resources[0] + steps * robots[0],
+                nhbr_resources[1] + steps * robots[1],
+                nhbr_resources[2] + steps * robots[2],
+                nhbr_resources[3] + steps * robots[3],
+            )
 
             if nhbr_time <= 0:
                 continue
@@ -104,11 +122,13 @@ def bfs(blueprint, resources, robots, maxtime):
                 nhbr_resources[3] + robots[3],
             )
             nhbr_robots = robots[:r] + (robots[r] + 1,) + robots[r + 1 :]
-            nhbr = (nhbr_time, nhbr_resources, nhbr_robots)
+
+            if r == 3:
+                nhbr_gas += nhbr_time
+            nhbr = (nhbr_time, nhbr_resources, nhbr_robots, nhbr_gas)
             if nhbr not in visited and estimax(nhbr) > best:
-                est = estimin(nhbr)
-                if est > best:
-                    best = est
+                if nhbr_gas > best:
+                    best = nhbr_gas
                 visited.add(nhbr)
                 queue.append(nhbr)
     return best
