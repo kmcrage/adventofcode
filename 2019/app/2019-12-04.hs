646ced2805e92aa123f06599099425c351fbec3a
kevin campbell <kevin.campbellrage@gmail.com>
2023-01-09 15:57:34 +1030

    day04: numeric filters

---
+++ 2019/app/2019-12-04.hs
@@ -0,0 +1,20 @@
+import Data.List (group, sort)
+
+
+main :: IO ()
+main = do
+    putStrLn $ "part 1: " ++ show result_1
+    putStrLn $ "part 2: " ++ show result_2
+    where
+        ordered_nums = filter ordered [171309..643603]
+        result_1 = length $ filter doubled ordered_nums
+        result_2 = length $ filter strictlyDoubled ordered_nums
+
+ordered :: Int -> Bool
+ordered n = show n == sort (show n)
+
+doubled :: Int -> Bool
+doubled = any ((>= 2) . length) . group . show
+
+strictlyDoubled :: Int -> Bool
+strictlyDoubled = any ((== 2) . length) . group . show
