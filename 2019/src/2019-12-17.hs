effc22fe5db0731827f385e5bbe2b8ed7812a70a
kevin campbell <kevin.campbellrage@gmail.com>
2023-01-29 16:21:57 +1030

    day18: bfs slow version

--- 2019/src/2019-12-17.hs
+++ 2019/src/2019-12-17.hs
@@ -29,7 +29,7 @@ main = do
   -- view dust
 
 view :: [Int] -> IO ()
-view d = putStrLn (map C.chr d) 
+view d = putStrLn (map C.chr d)
 
 findRoute :: Coord -> Int -> S.Set Coord -> [String] -> [String]
 findRoute (i, j) dir pts rt
@@ -37,7 +37,8 @@ findRoute (i, j) dir pts rt
   | otherwise = findRoute pos' dir' pts rt'
   where
     dirmap = M.fromList $ zip [0 ..] directions
-    turns = filter
+    turns =
+      filter
         (\(_, d) ->
            let (di, dj) = dirmap M.! d
             in (i + di, j + dj) `S.member` pts) $
@@ -53,18 +54,22 @@ findRoute (i, j) dir pts rt
 toMessage :: ([String], [[String]]) -> [Int]
 toMessage (ms, ws) = message'
   where
-    message = map (L.intersperse ",") >>> map toNums >>> L.intersperse [10] >>> concat $ [ms] ++ ws
+    message =
+      map (L.intersperse ",") >>> map toNums >>> L.intersperse [10] >>> concat $
+      [ms] ++ ws
     message' = message ++ [10, 110, 10] -- video feed
-    
+
 toNums :: [String] -> [Int]
-toNums = map (C.ord) . concat 
+toNums = map (C.ord) . concat
+
 {-  | C.isUpper c = C.ord c
   | otherwise = read s :: Int
   where
     c = fst . head . C.readLitChar $ s -}
-directions ::[Coord]
-directions = [(0, -1), (1,0), (0,1), (-1, 0)]
+directions :: [Coord]
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     -- list is map C.ord ['^', '>', 'v', '<'] = [94,62,118,60]
+
 {-
 
 test = ["R","8","R","8","R","4","R","4","R","8","L","6","L","2","R","4","R","4","R","8","R","8","R","8","L","6","L","2"]
@@ -112,8 +117,8 @@ cross :: S.Set Coord -> S.Set Coord
 cross pts = S.filter ((== 4) . length . npts) pts
   where
     npts (i, j) =
-      map (\(di, dj) -> (i + di, j + dj)) >>> filter (`S.member` pts) $ directions
-    
+      map (\(di, dj) -> (i + di, j + dj)) >>> filter (`S.member` pts) $
+      directions
 
 sumAlign :: S.Set Coord -> Int
 sumAlign = foldr (\(i, j) s -> i * j + s) 0
