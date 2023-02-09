6127d249f6c62458ff2d6ddd0d11f6de6bc313f4
kevin campbell <kevin.campbellrage@gmail.com>
2023-02-09 15:28:49 +1030

    day02:tidy

--- 2018/src/2018-12-02.hs
+++ 2018/src/2018-12-02.hs
@@ -17,20 +17,19 @@ main = do
   putStrLn $ "Part 1, product: " ++ show part1
   putStrLn $ "Part 2, match: " ++ show part2
 
-
-
+--     [things to test] -> current -> [to test against current]
 nearest :: [String] -> String -> [String] -> String
-nearest [] _ [] = "fail"
-nearest (a:as) _ [] = nearest as a as
-nearest ws a (b:bs) 
-  | near a b = result
-  | otherwise = nearest ws a bs
-  where 
-    result = filter (uncurry (==)) >>> map fst $ zip a b 
+nearest [] _ [] = error "fail"
+nearest (a:as) _ [] = nearest as a as -- set up to test a
+nearest ws a (b:bs)
+  | near a b = result -- success!
+  | otherwise = nearest ws a bs -- tested one possible match against a
+  where
+    result = filter (uncurry (==)) >>> map fst $ zip a b
 
-near :: String -> String -> Bool 
-near a b 
-  | diff==1 = True
-  | otherwise = False 
-  where 
+near :: String -> String -> Bool
+near a b
+  | diff == 1 = True
+  | otherwise = False
+  where
     diff = filter (uncurry (/=)) >>> length $ zip a b
