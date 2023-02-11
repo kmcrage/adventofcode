1a1ef7399e42c6c69a8d515c0df7a73cd588c096
kevin campbell <kevin.campbellrage@gmail.com>
2023-02-11 22:15:34 +1030

    day06: optimised

--- 2018/src/2018-12-06.hs
+++ 2018/src/2018-12-06.hs
@@ -10,6 +10,8 @@ import           Debug.Trace
 
 type Coord = (Int, Int)
 
+type BBox = (Int, Int, Int, Int)
+
 main :: IO ()
 main = do
   contents <- readFile "data/2018-12-06.dat"
@@ -20,10 +22,10 @@ main = do
   putStrLn $ "Part 1, min: " ++ show part1
   putStrLn $ "Part 2, sum: " ++ show part2
 
-nearbyArea :: Int -> [Coord] -> Int 
+nearbyArea :: Int -> [Coord] -> Int
 nearbyArea mx pts = paintMax mx pts S.empty [c]
-  where 
-    c = avePt pts 
+  where
+    c = avePt pts
 
 paintMax :: Int -> [Coord] -> S.Set Coord -> [Coord] -> Int
 paintMax _ _ visited [] = length visited
@@ -39,33 +41,37 @@ paintMax mx points visited (q:queue)
       map (\(di, dj) -> (i + di, j + dj)) >>> filter (`S.notMember` visited) $
       [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-avePt :: [Coord] -> Coord 
+avePt :: [Coord] -> Coord
 avePt pts = (div a l, div b l)
-  where 
-    (a,b) = L.foldl' (\(i,j) (k,l) -> (i+k,j+l)) (0,0) pts
+  where
+    (a, b) = L.foldl' (\(i, j) (k, l) -> (i + k, j + l)) (0, 0) pts
     l = length pts
 
 maxFiniteArea :: [Coord] -> Int
-maxFiniteArea points = area points 0 points
+maxFiniteArea points = area (bbXmin, bbYmin, bbXmax, bbYmax) points 0 points
+  where
+    bbXmin = map fst >>> minimum $ points
+    bbYmin = map snd >>> minimum $ points
+    bbXmax = map fst >>> maximum $ points
+    bbYmax = map snd >>> maximum $ points
 
 -- full list, max area, queue
-area :: [Coord] -> Int -> [Coord] -> Int
-area _ mx [] = mx
-area points mx (p:queue) = area points mx' queue
+area :: BBox -> [Coord] -> Int -> [Coord] -> Int
+area _ _ mx [] = mx
+area bbox points mx (p:queue) = area bbox points mx' queue
   where
-    a = paint mxDist p points S.empty [p]
-    mx' = max a mx
-    mxDist = map (manDist p) >>> maximum $ points
+    mx' = max mx $ paint bbox p points S.empty [p]
 
 -- maxDist, current point, all points, area coords, queue -> area
-paint :: Int -> Coord -> [Coord] -> S.Set Coord -> [Coord] -> Int
+paint :: BBox -> Coord -> [Coord] -> S.Set Coord -> [Coord] -> Int
 paint _ _ _ visited [] = length visited
-paint mx p points visited (q:queue)
-  | S.member q visited = paint mx p points visited queue
-  | qp < qmin = paint mx p points visited' (queue ++ nhbrs)
-  | qp > mx = 0
-  | otherwise = paint mx p points visited queue
+paint bbox p points visited (q:queue)
+  | S.member q visited = paint bbox p points visited queue
+  | qp < qmin = paint bbox p points visited' (queue ++ nhbrs)
+  | i == imn || i == imx || j == jmn || j == jmx = 0
+  | otherwise = paint bbox p points visited queue
   where
+    (imn, jmn, imx, jmx) = bbox
     qp = manDist q p
     qmin = filter (/= p) >>> map (manDist q) >>> minimum $ points
     visited' = S.insert q visited
