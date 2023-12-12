59ed07783a24a1fb8e25919ec3a95b4c5c005c3a
kevin campbell <kevin.campbellrage@gmail.com>
2023-12-12 20:01:17 +1030

    day12: dynamic prog soln

--- 2023/2023-12-12/main.go
+++ 2023/2023-12-12/main.go
@@ -10,147 +10,90 @@ import (
 )
 
 type Node struct {
-	Spring     string
-	NumQueries int
+	Spring    string
+	NumGroups int // how far through the target []int we are
+	HashCount int //how many #s in the current string
 }
 
-func (node Node) counter() []int {
-	cnt := 0
-	cnts := make([]int, 0)
-	for _, c := range node.Spring {
-		switch c {
-		case '.':
-			if cnt > 0 {
-				cnts = append(cnts, cnt)
-			}
-			cnt = 0
-		case '#':
-			cnt += 1
-		case '?':
-			if cnt > 0 {
-				cnts = append(cnts, cnt)
-			}
-			return cnts
-		}
-	}
-	if cnt > 0 {
-		cnts = append(cnts, cnt)
-	}
-	return cnts
-}
-
-func IntArrayEquals(a []int, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func parseLine(line string, repeats int) (Node, []int) {
+func parseLine(line string, repeats int) (string, []int) {
 	fields := strings.Fields(line)
 
-	springs := strings.Repeat("?"+fields[0], repeats)
-	springs = springs[1:]
-
-	cntsStr := strings.Repeat(","+fields[1], repeats)
-	cntsStr = cntsStr[1:]
-
-	cnts := strings.Split(cntsStr, ",")
-	counts := make([]int, len(cnts))
+	springs := strings.Repeat("?"+fields[0], repeats)[1:]
+	cnts := strings.Split(strings.Repeat(","+fields[1], repeats)[1:], ",")
+	targets := make([]int, len(cnts))
 	for i, sz := range cnts {
-		counts[i], _ = strconv.Atoi(sz)
+		targets[i], _ = strconv.Atoi(sz)
 	}
 
-	numQueries := 0
-	for _, s := range springs {
-		if s == '?' {
-			numQueries++
-		}
-	}
-
-	return Node{Spring: springs, NumQueries: numQueries}, counts
+	return springs, targets
 }
+
+var cache map[Node]int
 
 func processLine(line string, repeats int) int {
 	start, target := parseLine(line, repeats)
-
-	q := []Node{start}
-	matches := 0
-	visited := map[Node]bool{}
-	for len(q) > 0 {
-		node := q[0]
-		q = q[1:] //dequeue first node in queue(fifo)
-		if _, ok := visited[node]; ok {
-			continue
-		}
-		visited[node] = true
-		// goal := node.counter()
-		// fmt.Println(node, goal, target)
-
-		for _, c := range ".#" {
-			qnode := node
-			qnode.Spring = strings.Replace(qnode.Spring, "?", string(c), 1)
-			qnode.NumQueries--
-			goal := qnode.counter()
-			if IntArrayEquals(goal, target) {
-				matches++
-				continue
-			}
-			if _, ok := visited[qnode]; !ok &&
-				qnode.NumQueries >= 0 &&
-				len(goal) <= len(target) {
-				appendFlag := true
-				if len(goal) > 0 && goal[len(goal)-1] > target[len(goal)-1] {
-					appendFlag = false
-				}
-				if len(goal) > 1 && goal[len(goal)-2] != target[len(goal)-2] {
-					appendFlag = false
-				}
-				if appendFlag {
-					q = append(q, qnode)
-				}
-			}
-		}
-	}
-	return matches
+	cache = make(map[Node]int, 0)
+	return solver(Node{start + ".", 0, 0}, target)
 }
 
-func process(file string, repeats int) (int, error) {
+func solver(node Node, target []int) int {
+	if s, ok := cache[node]; ok {
+		return s
+	}
+
+	cache[node] = 0
+	if node.Spring == "" {
+		if len(target) == 0 && node.HashCount == 0 {
+			cache[node] = 1
+		}
+		return cache[node]
+	}
+
+	switch node.Spring[0] {
+	case '.':
+		if node.HashCount == 0 {
+			cache[node] = solver(Node{node.Spring[1:], node.NumGroups, 0}, target)
+		} else if node.HashCount == target[0] {
+			cache[node] = solver(Node{node.Spring[1:], node.NumGroups + 1, 0}, target[1:])
+		}
+	case '#':
+		if len(target) > 0 && node.HashCount < target[0] {
+			cache[node] = solver(Node{node.Spring[1:], node.NumGroups, node.HashCount + 1}, target)
+		}
+	case '?':
+		cache[node] = (solver(Node{"." + node.Spring[1:], node.NumGroups, node.HashCount}, target) +
+			solver(Node{"#" + node.Spring[1:], node.NumGroups, node.HashCount}, target))
+
+	}
+	return cache[node]
+}
+
+func process(file string, repeats []int) ([]int, error) {
+	sums := make([]int, len(repeats))
+
 	f, err := os.Open(file)
 	if err != nil {
-		return 0, err
+		return sums, err
 	}
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	sum := 0
 	for scanner.Scan() {
-		//println(sum)
-		sum += processLine(scanner.Text(), repeats)
-		// fmt.Println(sum)
-	}
-	return sum, nil
+		for i, repeat := range repeats {
+			sums[i] += processLine(scanner.Text(), repeat)
+		}
+	}	
+	return sums, nil
 }
 
 func main() {
 	// file := "test.dat"
 	file := "2023-12-12.dat"
 
-	part1, err := process(file, 1)
+	parts, err := process(file, []int{1, 5})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("part 1:", part1)
-	/*
-		part2, err := process(file, 5)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("part 2:", part2)
-	*/
+	fmt.Println("part 1:", parts[0])
+	fmt.Println("part 2:", parts[1])
 }
