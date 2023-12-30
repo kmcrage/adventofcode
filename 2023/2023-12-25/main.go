7c95cdb13c8f519f552ac8b1fc9f2c69f06e897d
kevin campbell <kevin.campbellrage@gmail.com>
2023-12-30 19:16:28 +1030

    day25: replace visual id with stochastic id

--- 2023/2023-12-25/main.go
+++ 2023/2023-12-25/main.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
 type NodeMap map[string]*Node
 type Wire struct {
 	from, to string
+	weight   int
 }
 type Node struct {
 	nghbrs map[string]bool
+	weight int
 }
 
 func parse(file string) ([]Wire, error) {
@@ -29,7 +32,7 @@ func parse(file string) ([]Wire, error) {
 		tokens := strings.Split(scanner.Text(), ": ")
 		from := tokens[0]
 		for _, to := range strings.Split(tokens[1], " ") {
-			wires = append(wires, Wire{from, to})
+			wires = append(wires, Wire{from: from, to: to})
 		}
 
 	}
@@ -41,7 +44,7 @@ func cut(wires []Wire, snips []Wire) []Wire {
 OUTER:
 	for _, wire := range wires {
 		for _, snip := range snips {
-			if snip == wire {
+			if snip.from == wire.from && snip.to == wire.to {
 				continue OUTER
 			}
 		}
@@ -54,10 +57,10 @@ func network(wires []Wire) NodeMap {
 	nodemap := make(NodeMap)
 	for _, wire := range wires {
 		if _, ok := nodemap[wire.from]; !ok {
-			nodemap[wire.from] = &Node{make(map[string]bool)}
+			nodemap[wire.from] = &Node{nghbrs: make(map[string]bool)}
 		}
 		if _, ok := nodemap[wire.to]; !ok {
-			nodemap[wire.to] = &Node{make(map[string]bool)}
+			nodemap[wire.to] = &Node{nghbrs: make(map[string]bool)}
 		}
 		nodemap[wire.from].nghbrs[wire.to] = true
 		nodemap[wire.to].nghbrs[wire.from] = true
@@ -111,11 +114,82 @@ func divisions(nodemap NodeMap) int {
 	return prod
 }
 
-func part1(wires []Wire, snips []Wire) int {
-	nodemap := network(cut(wires, snips))
-	return divisions(nodemap)
+func (nodemap NodeMap) weight(seed string,w int) {
+	fill := make(NodeMap)
+	queue := []string{seed}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		fill[node] = nodemap[node]
+		for nghbr := range nodemap[node].nghbrs {
+			if _, ok := fill[nghbr]; ok {
+				continue
+			}
+			// use a large step for the weight because we are
+			// averaging with ints
+			nodemap[nghbr].weight = nodemap[node].weight + w
+			queue = append(queue, nghbr)
+		}
+	}
 }
 
+func (nodemap NodeMap) blur(num int) {
+	for i := 0; i < num; i++ {
+		for node := range nodemap {
+			sum := 0
+			for nghbr := range nodemap[node].nghbrs {
+				sum += nodemap[nghbr].weight
+			}
+			nodemap[node].weight = sum / len(nodemap[node].nghbrs)
+		}
+	}
+}
+
+func (nodemap NodeMap) toWires() []Wire {
+	snips := make([]Wire, 0)
+	for node := range nodemap {
+		for nghbr := range nodemap[node].nghbrs {
+			weight := abs(nodemap[node].weight - nodemap[nghbr].weight)
+			snips = append(snips, Wire{node, nghbr, weight})
+		}
+	}
+	slices.SortFunc(snips, func(a Wire, b Wire) int {
+		if a.weight < b.weight {
+			return 1
+		} else if a.weight > b.weight {
+			return -1
+		}
+		return 0
+	})
+	return snips
+}
+
+func (nodemap NodeMap) bridges() []Wire {
+	var seed string
+	for seed = range nodemap {
+		break
+	}
+	nodemap.weight(seed, 100)
+	nodemap.blur(3)
+	snips := nodemap.toWires()
+	fmt.Println("snips:", snips[:10])
+	return snips[:6]
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func part1(wires []Wire) int {
+	nodemap := network(wires)
+	snips := nodemap.bridges()
+	return divisions(network(cut(wires, snips)))
+}
+
+/*
 func writedot(file string, wires []Wire) {
 	fo, err := os.Create(file)
 	if err != nil {
@@ -129,6 +203,7 @@ func writedot(file string, wires []Wire) {
 	fo.WriteString("}\n")
 
 }
+*/
 
 func main() {
 	// file := "test.dat"
@@ -138,14 +213,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	/*
 	writedot("2023-12-25.dot", wires)
 	// neato 2023-12-25.dot -O -Tjpg
 	// these wires found by obesrvation
-	snips := []Wire{
-		{"xft", "pzv"},
-		{"dqf", "cbx"},
-		{"sds", "hbr"},
-	}
-	fmt.Println("Part1:", part1(wires, snips))
+	/*
+		snips := []Wire{
+			{"xft", "pzv", 0},
+			{"dqf", "cbx", 0},
+			{"sds", "hbr", 0},
+		}
+	*/
+	fmt.Println("Part1:", part1(wires))
 
 }
