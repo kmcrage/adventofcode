1c54f8bb2743b15d2dbbcb8337931b3debfd205b
kevin campbell <kevin.campbellrage@gmail.com>
2024-12-01 18:26:47 +1030

    day01: more rustic

--- 2024/src/bin/2024-12-01.rs
+++ 2024/src/bin/2024-12-01.rs
@@ -1,26 +1,38 @@
-use scan_rules::*;
+use std::collections::HashMap;
+use std::fs::read_to_string;
 
-// const INPUT: &str = include_str!("../../inputs/test.txt");
-const INPUT: &str = include_str!("../../inputs/2024-12-01.txt");
+fn parse(file: &str) -> (Vec<usize>, Vec<usize>, HashMap<usize, usize>) {
+    let input = read_to_string(file).unwrap();
 
-fn main() {
-    let (mut x, mut y): (Vec<_>, Vec<_>) = INPUT
+    let mut freq: HashMap<usize, usize> = HashMap::new();
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input
         .lines()
         .map(|line| {
-            let_scan!(line; (let a:usize, " ", let b:usize));
-            (a, b)
+            let mut tokens = line.split_whitespace();
+            let l = tokens.next().unwrap().parse::<usize>().unwrap();
+            let r = tokens.next().unwrap().parse::<usize>().unwrap();
+            *freq.entry(r).or_default() += 1;
+            (l, r)
         })
         .unzip();
-    x.sort();
-    y.sort();
 
-    let part1: usize = x.iter().zip(y.iter()).map(|(&a, &b)| a.abs_diff(b)).sum();
+    left.sort();
+    right.sort();
 
+    (left, right, freq)
+}
+
+fn main() {
+    let file = "./inputs/2024-12-01.txt";
+    let (left, right, freq) = parse(file);
+
+    let part1: usize = left
+        .iter()
+        .zip(right.iter())
+        .map(|(&l, &r)| l.abs_diff(r))
+        .sum();
     println!("part1: {}", part1);
 
-    let part2: usize = x
-        .iter()
-        .map(|&a| a * y.iter().filter(|&n| *n == a).count())
-        .sum();
+    let part2: usize = left.iter().map(|l| l * freq.get(l).unwrap_or(&0)).sum();
     println!("part2: {}", part2);
 }
