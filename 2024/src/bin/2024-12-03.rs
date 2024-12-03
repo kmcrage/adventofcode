95810648b93618f54d084a0ead1c6c6c1957dbce
kevin campbell <kevin.campbellrage@gmail.com>
2024-12-03 18:41:01 +1030

    day03: fix edge case of no final do()

--- 2024/src/bin/2024-12-03.rs
+++ 2024/src/bin/2024-12-03.rs
@@ -11,13 +11,13 @@ fn calculate(input: &str) -> i32 {
 
 fn main() {
     let file = "./inputs/2024-12-03.txt";
-    //let file = "./inputs/test.txt";
+    // let file = "./inputs/test.txt";
 
     let input = read_to_string(file).unwrap_or_else(|_| panic!("Failed to read file: {}", file));
     println!("part1: {}", calculate(input.as_str()));
 
     // (?s): . matches \n
-    let redacted = Regex::new(r"(?s)don\'t\(\).*?do\(\)")
+    let redacted = Regex::new(r"(?s)don\'t\(\).*?(do\(\)|$)")
         .unwrap()
         .replace_all(input.as_str(), "");
     println!("part2: {}", calculate(&redacted));
