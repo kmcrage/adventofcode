4cd14d34fc96856733d2da55ba0069f62c1710c3
kevin campbell <kevin.campbellrage@gmail.com>
2024-12-19 17:23:55 +1030

    day19: fixup

--- 2024/src/bin/2024-12-19.rs
+++ 2024/src/bin/2024-12-19.rs
@@ -2,59 +2,46 @@ use hashbrown::HashMap;
 use regex::Regex;
 use std::fs::read_to_string;
 
-#[derive(Clone, Debug)]
-struct Towels {
-    towels: Vec<String>,
-    patterns: Vec<String>,
-    cache: HashMap<String, usize>,
+fn from(input: &str) -> (Vec<String>, Vec<String>) {
+    let (in_a, in_b) = input.split_once("\n\n").unwrap();
+
+    (
+        in_b.lines().map(String::from).collect(),
+        in_a.split(", ").map(String::from).collect(),
+    )
 }
 
-impl Towels {
-    fn from(input: &str) -> Self {
-        let (in_a, in_b) = input.split_once("\n\n").unwrap();
+fn simple_matches(towels: &[String], patterns: &[String]) -> usize {
+    let regex = format!("^({})*$", patterns.join("|"));
+    let re = Regex::new(&regex).unwrap();
 
-        Towels {
-            patterns: in_a.split(", ").map(String::from).collect(),
-            cache: HashMap::new(),
-            towels: in_b.lines().map(String::from).collect(),
-        }
+    towels.iter().filter(|towel| re.is_match(*towel)).count()
+}
+
+fn total_matches(towels: &[String], patterns: &[String]) -> usize {
+    let mut cache: HashMap<String, usize> = HashMap::new();
+    towels
+        .iter()
+        .map(|towel| matches_dp(towel, patterns, &mut cache))
+        .sum()
+}
+
+fn matches_dp(towel: &str, patterns: &[String], cache: &mut HashMap<String, usize>) -> usize {
+    if towel.is_empty() {
+        return 1;
+    }
+    if let Some(&result) = cache.get(towel) {
+        return result;
     }
 
-    fn simple_matches(&self) -> usize {
-        let regex = format!("^({})*$", self.patterns.join("|"));
-        let re = Regex::new(&regex).unwrap();
-
-        self.towels
-            .iter()
-            .filter(|towel| re.is_match(towel))
-            .count()
-    }
-
-    fn total_matches(&self) -> usize {
-        let mut p = self.clone();
-        self.towels
-            .iter()
-            .map(|towel| p.matches_dp(towel.as_str()))
-            .sum()
-    }
-
-    fn matches_dp(&mut self, towel: &str) -> usize {
-        if towel.is_empty() {
-            return 1;
+    let mut cnt = 0;
+    for pattern in patterns {
+        if towel.starts_with(pattern) {
+            cnt += matches_dp(&towel[pattern.len()..], patterns, cache);
         }
-        if let Some(&result) = self.cache.get(towel) {
-            return result;
-        }
-
-        let mut cnt = 0;
-        for pattern in self.patterns.clone() {
-            if towel.starts_with(&pattern) {
-                cnt += self.matches_dp(&towel[pattern.len()..]);
-            }
-        }
-        self.cache.insert(towel.to_string(), cnt);
-        cnt
     }
+    cache.insert(towel.to_string(), cnt);
+    cnt
 }
 
 fn main() {
@@ -62,7 +49,7 @@ fn main() {
     let file = "./inputs/2024-12-19.txt";
     let input = read_to_string(file).unwrap_or_else(|_| panic!("Failed to read file: {}", file));
 
-    let towels = Towels::from(input.as_str());
-    println!("part1: {:?}", towels.simple_matches());
-    println!("part1: {:?}", towels.total_matches());
+    let (towels, patterns) = from(input.as_str());
+    println!("part1: {:?}", simple_matches(&towels, &patterns));
+    println!("part1: {:?}", total_matches(&towels, &patterns));
 }
