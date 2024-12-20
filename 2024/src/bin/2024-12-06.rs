220904dc4ea069589b74d860978db028b15cb8b7
kevin campbell <kevin.campbellrage@gmail.com>
2024-12-20 14:32:02 +1030

    day06: optimisation

--- 2024/src/bin/2024-12-06.rs
+++ 2024/src/bin/2024-12-06.rs
@@ -7,39 +7,47 @@ struct State {
     grid: Vec<char>,
     dir: usize,
     pos: isize,
-    width: usize,
+    direction: [isize; 4],
 }
-
-const DIRECTION: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
 impl State {
     fn from(input: String) -> State {
+        let width = input.lines().next().unwrap().len() as isize + 1;
         State {
             grid: input.chars().collect(),
             dir: 0,
             pos: input.chars().position(|c| c == '^').unwrap() as isize,
-            width: input.lines().next().unwrap().len() + 1, // extra char for edge of grid
+            direction: [-width, 1, width, -1],
         }
     }
 
-    fn walk(&self) -> Option<HashSet<isize>> {
+    // if brick == None, we want a full path to use to decide where to put bricks
+    //
+    // If brick == Some, we only need check the brick positions in dir 0 for loops
+    //
+    fn walk(&self, brick: Option<isize>) -> Option<HashSet<isize>> {
         let mut pos = self.pos;
         let mut dir = self.dir;
         let mut visited: HashSet<(isize, usize)> = Default::default();
 
         loop {
-            // steps += 1;
-            visited.insert((pos, dir));
-
-            let next_pos = pos + (self.width as isize) * DIRECTION[dir].0 + DIRECTION[dir].1;
-
-            if visited.contains(&(next_pos, dir)) {
-                break None; //this is a loop
+            let next_pos = pos + self.direction[dir];
+            if brick.is_none() {
+                visited.insert((pos, dir));
+                if visited.contains(&(next_pos, dir)) {
+                    break None; //this is a loop
+                }
             }
 
             if let Some(&next_chr) = self.grid.get(next_pos as usize) {
-                if next_chr == '#' {
+                if next_chr == '#' || (brick.is_some() && next_pos == brick.unwrap()) {
                     dir = (dir + 1) % 4;
+                    if brick.is_some() && dir == 0 {
+                        if visited.contains(&(next_pos, dir)) {
+                            break None; //this is a loop
+                        }
+                        visited.insert((next_pos, dir));
+                    }
                 } else if next_chr == '\n' {
                     // off the side of the grid
                     break Some(visited.iter().map(|(p, _)| *p).collect());
@@ -55,12 +63,12 @@ impl State {
 }
 
 fn part1(origin: &State) -> usize {
-    origin.walk().unwrap().len()
+    origin.walk(None).unwrap().len()
 }
 
 fn part2(origin: &State) -> usize {
     origin
-        .walk()
+        .walk(None)
         .unwrap()
         .par_iter()
         .filter(|pos| {
@@ -69,10 +77,7 @@ fn part2(origin: &State) -> usize {
                 return false;
             }
 
-            let mut state = origin.clone();
-            state.grid[**pos as usize] = '#';
-
-            match state.walk() {
+            match origin.walk(Some(**pos)) {
                 Some(_v) => false,
                 None => true,
             }
