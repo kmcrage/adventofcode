9064e17d99f46cac38934c695fa891ccb529008e
kevin campbell <kevin.campbellrage@gmail.com>
2024-12-24 12:39:59 +1030

    day23: part2, clique finding

--- 2024/src/bin/2024-12-23.rs
+++ 2024/src/bin/2024-12-23.rs
@@ -61,10 +61,86 @@ fn trios(network: &Network, chr: char, len: usize) -> usize {
     result.len()
 }
 
+/*
+https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+
+given three disjoint sets of vertices R, P, and X, it finds the
+maximal cliques that include all of the vertices in R,
+some of the vertices in P, and none of the vertices in X
+
+algorithm BronKerbosch1(R, P, X) is
+    if P and X are both empty then
+        report R as a maximal clique
+    for each vertex v in P do
+        BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+        P := P \ {v}
+        X := X ⋃ {v}
+
+algorithm BronKerbosch2(R, P, X) is
+    if P and X are both empty then
+        report R as a maximal clique
+    choose a pivot vertex u in P ⋃ X
+    for each vertex v in P \ N(u) do
+        BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+        P := P \ {v}
+        X := X ⋃ {v}
+*/
+
+fn bron_kerbosch2<'a>(
+    network: &'a Network,
+    r: HashSet<&'a str>,
+    mut p: HashSet<&'a str>,
+    mut x: HashSet<&'a str>,
+    cliques: &mut Vec<HashSet<&'a str>>,
+) {
+    if p.is_empty() {
+        if x.is_empty() {
+            cliques.push(r);
+        }
+        return;
+    }
+
+    let nodes = p.clone();
+    let pivot = *p.union(&x).max_by_key(|&&v| network[v].len()).unwrap();
+    nodes.difference(&network[pivot]).for_each(|v| {
+        let nhbd_v = network.get(v).unwrap();
+        let v_set = HashSet::from([*v]);
+        bron_kerbosch2(
+            network,
+            r.union(&v_set).cloned().collect(),
+            p.intersection(nhbd_v).cloned().collect(),
+            x.intersection(nhbd_v).cloned().collect(),
+            cliques,
+        );
+        p.remove(v);
+        x.insert(*v);
+    })
+}
+
+fn maximal_clique(network: &Network) -> String {
+    let r = HashSet::new();
+    let p = HashSet::from_iter(network.keys().cloned());
+    let x = HashSet::new();
+    let mut cliques: Vec<HashSet<&str>> = Default::default();
+    bron_kerbosch2(network, r, p, x, &mut cliques);
+
+    let mut maximal = cliques
+        .iter()
+        .max_by_key(|c| c.len())
+        .unwrap()
+        .into_iter()
+        .cloned()
+        .map(String::from)
+        .collect::<Vec<_>>();
+    maximal.sort();
+    maximal.join(",")
+}
+
 fn main() {
-    let file = "./inputs/test.txt";
-    // let file = "./inputs/2024-12-23.txt";
+    // let file = "./inputs/test.txt";
+    let file = "./inputs/2024-12-23.txt";
     let input = read_to_string(file).unwrap_or_else(|_| panic!("Failed to read file: {}", file));
     let network = from(input.as_str());
     println!("part1: {}", trios(&network, 't', 3));
+    println!("part2: {:?}", maximal_clique(&network));
 }
