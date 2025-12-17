88c3a97da7333e2494964b58c1d48107ae7487e0
kevin campbell <kevin.campbellrage@gmail.com>
2025-12-17 15:15:45 +1030

    [2025-12-10] recursive mathod

--- 2025/src/bin/2025-12-10.rs
+++ 2025/src/bin/2025-12-10.rs
@@ -1,14 +1,10 @@
-use good_lp::{highs, Expression, IntoAffineExpression, Solution, SolverModel, variable, variables};
-use hashbrown::HashSet;
+use hashbrown::HashMap;
 use std::cmp::min;
-use std::collections::VecDeque;
 use std::fs::read_to_string;
+use rayon::iter::{ParallelBridge, ParallelIterator};
 
-#[derive(Clone, Debug)]
-struct State {
-    lights: Vec<bool>,
-    depth: usize,
-}
+type CacheTP = HashMap<Vec<bool>, Vec<(usize, Vec<usize>)>>;
+type CacheJ = HashMap<Vec<usize>, usize>;
 
 fn parse(input: &str) -> (Vec<bool>, Vec<Vec<usize>>, Vec<usize>) {
     let tokens = input.split_whitespace().collect::<Vec<_>>();
@@ -37,47 +33,10 @@ fn parse(input: &str) -> (Vec<bool>, Vec<Vec<usize>>, Vec<usize>) {
     (goal, buttons, joltage)
 }
 
-fn bfs(start: State, target: &[bool], buttons: &[Vec<usize>]) -> usize {
-    let mut queue = VecDeque::from([start]);
-    let mut seen = HashSet::new();
-
-    while let Some(state) = queue.pop_front() {
-        if seen.contains(&state.lights) {
-            continue;
-        }
-        seen.insert(state.lights.clone());
-        for presses in buttons {
-            let next = &mut state.clone();
-            next.depth += 1;
-            for &press in presses {
-                next.lights[press] = !next.lights[press];
-            }
-            if next.lights == target {
-                return next.depth;
-            }
-            queue.push_back(next.clone());
-        }
-    }
-    0
-}
-
 fn part1(input: &str) -> usize {
     input
         .lines()
-        .map(|line| {
-            let (target, buttons, _) = parse(line);
-            let start = State {
-                lights: (0..target.len()).map(|_| false).collect(),
-                depth: 0,
-            };
-            bfs(start, &target, &buttons)
-        })
-        .sum()
-}
-
-fn part1a(input: &str) -> usize {
-    input
-        .lines()
+        .par_bridge()
         .map(|line| {
             let (target, buttons, _) = parse(line);
             let mut min_cost = usize::MAX;
@@ -105,35 +64,87 @@ fn part1a(input: &str) -> usize {
         .sum()
 }
 
-fn solve_ilp(buttons: &[Vec<usize>], joltages: &[usize]) -> usize {
-    let mut vars = variables!();
-    let press_vars = (0..buttons.len())
-        .map(|_| vars.add(variable().min(0).integer()))
-        .collect::<Vec<_>>();
+fn target_presses(
+    target: &[bool],
+    buttons: &[Vec<usize>],
+    cache: &mut CacheTP,
+) -> Vec<(usize, Vec<usize>)> {
+    let key = target.to_vec();
+    if cache.contains_key(&key) {
+        return cache[&key].clone();
+    }
 
-    let mut problem = highs(vars.minimise(press_vars.iter().sum::<Expression>()));
-    let mut exprs = vec![0.into_expression(); joltages.len()];
-    for (i, button) in buttons.iter().enumerate() {
-        for &x in button {
-            exprs[x] += press_vars[i];
+    let mut result = Vec::new();
+    for sol in 0..2_usize.pow(buttons.len() as u32) {
+        let mut soln = sol;
+        let mut presses = (0..target.len()).map(|_| 0_usize).collect::<Vec<_>>();
+        for button in buttons {
+            if !soln.is_multiple_of(2) {
+                for &light in button.iter() {
+                    presses[light] += 1;
+                }
+            }
+            soln /= 2;
         }
+        if presses
+            .iter()
+            .map(|j| !j.is_multiple_of(2))
+            .zip(target)
+            .any(|(j, t)| j != *t)
+        {
+            continue;
+        }
+        result.push((sol.count_ones() as usize, presses));
     }
-    for (e, &j) in exprs.into_iter().zip(joltages) {
-        problem.add_constraint(e.eq(j as f64));
+    cache.insert(key, result.clone());
+    result
+}
+
+fn solve_recursive(
+    buttons: &[Vec<usize>],
+    joltages: &[usize],
+    cache_tp: &mut CacheTP,
+    cache: &mut CacheJ,
+) -> usize {
+    let key = joltages.to_vec();
+    if cache.contains_key(&key) {
+        return cache[&key];
     }
-    let sol = problem.solve().unwrap();
-    press_vars
+    if joltages.iter().all(|j| *j == 0) {
+        return 0;
+    }
+
+    let target = joltages
         .iter()
-        .map(|&v| sol.value(v).round() as usize)
-        .sum()
+        .map(|j| !j.is_multiple_of(2))
+        .collect::<Vec<_>>();
+    let mut min_cost = usize::MAX / 3;
+    for (cst, presses) in target_presses(&target, buttons, cache_tp) {
+        if presses.iter().zip(joltages).any(|(a, b)| a > b) {
+            continue;
+        }
+
+        let next_joltages = joltages
+            .iter()
+            .zip(presses)
+            .map(|(j, d)| (j - d) / 2)
+            .collect::<Vec<_>>();
+        let cost = cst + 2 * solve_recursive(buttons, &next_joltages, cache_tp, cache);
+        min_cost = min(min_cost, cost);
+    }
+    cache.insert(key, min_cost);
+    min_cost
 }
 
 fn part2(input: &str) -> usize {
     input
         .lines()
+        .par_bridge()
         .map(|line| {
             let (_, buttons, joltages) = parse(line);
-            solve_ilp(&buttons, &joltages)
+            let mut cache_tp = HashMap::new();
+            let mut cache_j = HashMap::new();
+            solve_recursive(&buttons, &joltages, &mut cache_tp, &mut cache_j)
         })
         .sum()
 }
@@ -143,6 +154,5 @@ fn main() {
     let input = read_to_string("./inputs/2025-12-10.txt").unwrap();
 
     println!("part1: {:?}", part1(&input));
-    println!("part1a: {:?}", part1a(&input));
     println!("part2: {:?}", part2(&input));
 }
