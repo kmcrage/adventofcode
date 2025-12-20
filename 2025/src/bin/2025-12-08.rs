7610774646bb4814f40ea3c4e461886fb3ea15ec
kevin campbell <kevin.campbellrage@gmail.com>
2025-12-20 12:48:28 +1030

    [2025-12-08] disjoint set union find

--- 2025/src/bin/2025-12-08.rs
+++ 2025/src/bin/2025-12-08.rs
@@ -1,59 +1,83 @@
 use binary_heap_plus::{BinaryHeap, MinComparator};
-use hashbrown::{HashMap, HashSet};
+// use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 use std::fs::read_to_string;
 
 type Point = (i64, i64, i64);
-type Pairs = BinaryHeap<(i64, Point, Point), MinComparator>;
+type Pairs = BinaryHeap<(u64, usize, usize), MinComparator>;
 
-fn dist(x: &Point, y: &Point) -> i64 {
-    (x.0 - y.0) * (x.0 - y.0) + (x.1 - y.1) * (x.1 - y.1) + (x.2 - y.2) * (x.2 - y.2)
+#[derive(Clone, Debug)]
+struct UnionFindNode {
+    point: Point,
+    parent: usize,
+    size: usize,
 }
 
-fn part1(pairs: &Pairs, num: usize) -> usize {
-    let mut connections: HashMap<Point, Vec<Point>> = HashMap::new();
-
-    for (_, start, end) in pairs.clone().into_iter_sorted().take(num) {
-        if let Some(v) = connections.get_mut(&start) {
-            v.push(end);
-        } else {
-            let v = vec![end];
-            connections.insert(start, v);
-        }
-        if let Some(v) = connections.get_mut(&end) {
-            v.push(start);
-        } else {
-            let v = vec![start];
-            connections.insert(end, v);
-        }
-    }
-
-    let mut sizes: BinaryHeap<usize> = BinaryHeap::new();
-    let mut circuit: HashSet<Point> = HashSet::new();
-    let mut queue;
-    while !connections.is_empty() {
-        queue = vec![*connections.keys().next().unwrap()];
-        circuit.clear();
-        while let Some(pt) = queue.pop() {
-            if circuit.contains(&pt) {
-                continue;
-            }
-            circuit.insert(pt);
-            queue.extend(connections.get(&pt).unwrap());
-            connections.remove(&pt);
-        }
-        sizes.push(circuit.len());
-    }
-    sizes.into_iter_sorted().take(3).product()
+#[derive(Clone, Debug)]
+struct UnionFind {
+    forest: Vec<UnionFindNode>,
 }
 
-fn parse(input: &str) -> Pairs {
-    let junctions: Vec<Point> = input
+impl UnionFind {
+    fn find(&mut self, n: usize) -> usize {
+        let mut x = n;
+        while self.forest[x].parent != x {
+            (x, self.forest[x].parent) = (
+                self.forest[x].parent,
+                self.forest[self.forest[x].parent].parent,
+            );
+        }
+        x
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let mut x = self.find(x);
+        let mut y = self.find(y);
+
+        if x == y {
+            return;
+        }
+
+        // If necessary, swap variables to ensure that
+        // x has at least as many descendants as y
+        if self.forest[x].size < self.forest[y].size {
+            (x, y) = (y, x);
+        }
+
+        // Make x the new root
+        self.forest[y].parent = x;
+        // Update the size of x
+        self.forest[x].size += self.forest[y].size;
+    }
+
+    fn areas(&self) -> BinaryHeap<usize> {
+        let areas = self.forest
+            .iter()
+            .enumerate()
+            .filter(|(i, node)| *i == node.parent)
+            .map(|(_, node)| node.size)
+            .collect::<Vec<_>>();
+        BinaryHeap::from_vec(areas)
+    }
+}
+
+fn dist(x: &Point, y: &Point) -> u64 {
+    x.0.abs_diff(y.0).pow(2) + x.1.abs_diff(y.1).pow(2) + x.2.abs_diff(y.2).pow(2)
+}
+
+fn parse(input: &str) -> (UnionFind, Pairs) {
+    let junctions: Vec<UnionFindNode> = input
         .lines()
         .flat_map(|line| {
             line.split(',')
                 .map(|n| n.parse::<i64>().unwrap())
                 .tuples::<(_, _, _)>()
+        })
+        .enumerate()
+        .map(|(i, pt)| UnionFindNode {
+            point: pt,
+            parent: i,
+            size: 1,
         })
         .collect();
 
@@ -63,67 +87,43 @@ fn parse(input: &str) -> Pairs {
         .flat_map(|(i, start)| {
             junctions
                 .iter()
-                .skip(i + 1)
-                .map(|end| (dist(start, end), *start, *end))
+                .enumerate()
+                .take(i)
+                .map(|(j, end)| (dist(&start.point, &end.point), i, j))
                 .collect::<Vec<_>>()
         })
         .collect();
-    BinaryHeap::from_vec(pairs)
+
+    (UnionFind { forest: junctions }, BinaryHeap::from_vec(pairs))
 }
 
-fn part2(pairs: &Pairs) -> i64 {
-    let pairs_len = pairs.len();
+fn part1(uf: &mut UnionFind, pairs: &Pairs, num: usize) -> usize {
+    for (_, start, end) in pairs.clone().into_iter_sorted().take(num) {
+        uf.union(start, end)
+    }
+
+    uf.areas().into_iter_sorted().take(3).product()
+}
+
+fn part2(uf: &mut UnionFind, pairs: &Pairs) -> i64 {
     let mut pairs = pairs.clone();
-
-    let mut connections: HashMap<Point, Vec<Point>> = HashMap::new();
-    let mut circuit: HashSet<Point> = HashSet::new();
     while let Some((_, start, end)) = pairs.pop() {
-        if let Some(v) = connections.get_mut(&start) {
-            v.push(end);
-        } else {
-            let v = vec![end];
-            connections.insert(start, v);
-        }
-        if let Some(v) = connections.get_mut(&end) {
-            v.push(start);
-        } else {
-            let v = vec![start];
-            connections.insert(end, v);
-        }
+        uf.union(start, end);
 
-        // this hasn't got enough connections to connect everything
-        if connections.len() * (connections.len() - 1) < 2 * pairs_len {
-            continue;
-        }
-        // the new connection doesn't add to the circuit
-        if !circuit.is_empty() && !circuit.contains(&start) && !circuit.contains(&end) {
-            continue;
-        }
-
-        let mut queue = vec![start, end];
-
-        while let Some(pt) = queue.pop() {
-            if circuit.contains(&pt) {
-                continue;
-            }
-            circuit.insert(pt);
-            queue.extend(connections.get(&pt).unwrap());
-        }
-        if circuit.len() * (circuit.len() - 1) == 2 * pairs_len {
-            return start.0 * end.0;
-        }
+        let root = uf.find(0);
+        if uf.forest[root].size == uf.forest.len(){
+            return uf.forest[start].point.0 * uf.forest[end].point.0;
+        }   
     }
     0
 }
 
+
 fn main() {
     //let input = read_to_string("./inputs/example.txt").unwrap();
     let input = read_to_string("./inputs/2025-12-08.txt").unwrap();
-    let pairs = parse(&input);
+    let (mut uf, pairs) = parse(&input);
 
-    //println!("part1: {}", part1(&pairs, 10));
-    //println!("part2: {}", part2(&pairs));
-
-    println!("part1: {}", part1(&pairs, 1000));
-    println!("part2: {}", part2(&pairs));
+    println!("part1: {}", part1(&mut uf, &pairs, 1000));
+    println!("part2: {}", part2(&mut uf, &pairs));
 }
